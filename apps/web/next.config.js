5a53d369fbcd29f703efdebec978a7d8c3aa99a0
alesberesik22 <beresik.ales@gmail.com>
2024-02-03 13:50:58 +0100

    update pipeline

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   transpilePackages: ["@repo/ui"],
-  output: 'export',
-  images: {
-    unoptimized: true
-  }
+  reactStrictMode: true,
+  output: "standalone",
 };
