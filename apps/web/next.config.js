a467c402fd27ea4b045fd85e13ce4ac8d5a2743b
alesberesik22 <beresik.ales@gmail.com>
2024-02-03 13:43:52 +0100

    next export config

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   transpilePackages: ["@repo/ui"],
-  output: 'standalone'
+  output: 'export',
+  images: {
+    unoptimized: true
+  }
 };
