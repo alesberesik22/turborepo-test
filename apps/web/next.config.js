84ec00c842fade41dd4330f6c8598ccafccbb582
alesberesik22 <beresik.ales@gmail.com>
2024-02-04 13:24:21 +0100

    change ci

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,17 +1,6 @@
 /** @type {import('next').NextConfig} */
-const path = require('path');
-
 module.exports = {
-  reactStrictMode: true,
   transpilePackages: ["@repo/ui"],
-  output: 'standalone',
-  experimental: {
-    // this includes files from the monorepo base two directories up
-    outputFileTracingRoot: path.join(__dirname, '../../'),
-    outputFileTracingIncludes: {
-      '/eslint-config-custom': ['./eslint-config-custom/**/*'],
-      '/tsconfig': ['./tsconfig/**/*'],
-      '/@repo/ui': ['./@repo/ui/**/*'],
-    }
-  },
+  reactStrictMode: true,
+  output: "standalone",
 };
